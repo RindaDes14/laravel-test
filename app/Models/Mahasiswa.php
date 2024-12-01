6b6348c5aab40a61eb3bca10407d43372bcbc955
RindaDes14 <rindadesfourtheen@gmail.com>
2024-12-01 11:22:14 +0700

    first commit

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    //cara mengkoneksi model dengan tabel
+    protected $table = 'mahasiswa';
+    //data kolom yang tidak boleh di isi
+    protected $guarded = [
+        'id'
+    ];
+    
+    //data kolom yang boleh di isi
+    // protected $fillable = [
+    //     'nama', 
+    //     'umur', 
+    //     'jenis_kelamin', 
+    //     'tinggi_badan',
+    //     'tahun',
+    //     'tanggal_lahir'
+    // ];
+}
